73c165c402b6d81b49a338d76a5b15ef90d85096
Fathomed <jeff@fathomsolutions.co.nz>
2024-02-29 15:14:11 +1300

    Updates including metadata

--- scripts/tusUploads.js
+++ scripts/tusUploads.js
@@ -5,25 +5,46 @@ const getExpiryDate = () => {
 };
 
 function startUpload(file, chunkSize, endpoint) {
+	const fileNameInputValue = document.getElementById('file-name').value || file.name;
+
 	const options = {
 		endpoint: endpoint,
 		chunkSize: chunkSize,
 		metadata: {
 			expiry: getExpiryDate(),
 			maxDurationSeconds: 3600,
-			name: file.name,
+			name: fileNameInputValue, // Use the value from the input or the file name
+			allowedOrigins: ['nzfishingworld.co.nz', 'www.nzfishingworld.co.nz', 'anglers-planet.webflow.io'],
 		},
-		onError(error) {},
-		onSuccess() {},
-		onProgress(bytesUploaded) {
+		onError(error) {
+			console.error('Upload failed:', error);
+		},
+		onSuccess() {
+			console.log('Upload finished');
+		},
+		onProgress(bytesUploaded, bytesTotal) {
 			const container = document.getElementById('progress-bar-container');
 			const progressBar = document.getElementById('progress-bar-fill');
 			const progressBarText = document.getElementById('progress-bar-percent');
-			const progress = (bytesUploaded / file.size) * 100;
+			const progress = (bytesUploaded / bytesTotal) * 100;
 
 			container.style.display = 'block';
 			progressBar.style.width = progress + '%';
 			progressBarText.innerHTML = Math.floor(progress) + '% Complete';
+		},
+		// This function is crucial for extracting the stream-media-id
+		onAfterResponse: function (req, res) {
+			return new Promise((resolve) => {
+				if (res.getStatus() === 204) {
+					const mediaIdHeader = res.getHeader('stream-media-id');
+					if (mediaIdHeader) {
+						console.log('Media ID:', mediaIdHeader); // Log the media ID or use it as needed
+
+						document.getElementById('vidoe-id').value = mediaIdHeader; // Update the input field with the retrieved media ID
+					}
+				}
+				resolve();
+			});
 		},
 	};
 
--- src/handler.ts
+++ src/handler.ts
@@ -10,27 +10,7 @@ declare global {
 	const AP_UPLOAD_TOKEN: string;
 }
 
-export async function handleBasicCreatorUpload(request: Request): Promise<Response> {
-	const result = await fetch(`https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/stream/direct_upload`, {
-		headers: {
-			Authorization: `Bearer ${AP_UPLOAD_TOKEN}`,
-		},
-		body: JSON.stringify({
-			maxDurationSeconds: 3600,
-			expiry: '2022-06-07T23:20:00Z',
-			requireSignedURLs: true,
-			allowedOrigins: ['*'],
-		}),
-		method: 'POST',
-	});
-
-	return new Response(JSON.stringify(await result.json(), null, 2), {
-		headers: {
-			...corsHeaders,
-			'content-type': 'application/json;charset=UTF-8',
-		},
-	});
-}
+// if required for small or basic uploads gos here. see https://github.com/Fathomed/Cloudflare-Stream-Video-Upload
 
 export async function handleTusCreatorUpload(request: Request): Promise<Response> {
 	if (request.method === 'OPTIONS' || request.method == 'HEAD') {
